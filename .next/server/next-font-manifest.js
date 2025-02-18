e94eb4025310b6a3335f58555871210888efa42d
Dani Castro <castrodaniel244@gmail.com>
2025-02-18 10:14:36 -0400

    ignoring .next folder

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\n  \"app\": {\n    \"[project]/src/app/page\": [\n      \"static/media/BebasNeue_Regular-s.p.5dfa9788.woff2\"\n    ]\n  },\n  \"appUsingSizeAdjust\": true,\n  \"pages\": {},\n  \"pagesUsingSizeAdjust\": false\n}"
